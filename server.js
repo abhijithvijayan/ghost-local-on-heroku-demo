5bebb3cdcd62c870ad3830bc28b3ff8e477c08ad
Alex Rothberg <agrothberg@gmail.com>
2019-04-16 03:01:07 -0400

    handle subdirs

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var ghost = require('ghost');
+var express = require('express');
+var urlService = require('./node_modules/ghost/core/server/services/url');
+var parentApp = express();
 
 // Run a single Ghost process
 ghost()
-  .then( ghostServer => ghostServer.start() )
+  .then(function (ghostServer) {
+    ////////////////////////////////////////////////////////////////
+    // this is what you need to get subdirectories working properly!
+    // e.g. https://www.website.com/blog
+    parentApp.use(urlService.utils.getSubdir(), ghostServer.rootApp);
+    ghostServer.start(parentApp);
+    ////////////////////////////////////////////////////////////////
+  })
   .catch( error => {
     console.error(`Ghost server error: ${error.message} ${error.stack}`);
     process.exit(1);
