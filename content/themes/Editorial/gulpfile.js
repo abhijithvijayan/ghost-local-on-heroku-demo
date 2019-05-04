1827b9d3b47d44edfa2259dcc7ab5518275cd0e3
Swastik Nath <swanth.project@gmail.com>
2019-05-04 02:28:43 +0000

    Add free themes: Editorial, London

---
+++ content/themes/Editorial/gulpfile.js
@@ -0,0 +1,66 @@
+const {series, watch, src, dest, parallel} = require('gulp');
+const pump = require('pump');
+
+// gulp plugins and utils
+var livereload = require('gulp-livereload');
+var sass = require('gulp-sass');
+var zip = require('gulp-zip');
+var beeper = require('beeper');
+
+function serve(done) {
+    livereload.listen();
+    done();
+}
+
+const handleError = (done) => {
+    return function (err) {
+        if (err) {
+            beeper();
+        }
+        return done(err);
+    };
+};
+
+sass.compiler = require('node-sass');
+
+function hbs(done) {
+    pump([
+        src(['*.hbs', 'partials/**/*.hbs', '!node_modules/**/*.hbs']),
+        livereload()
+    ], handleError(done));
+}
+
+function css(done) {
+    pump([
+        src('./assets/main/sass/*.scss', {sourcemaps: true}),
+        sass({outputStyle: 'compressed'}).on('error', sass.logError),
+        dest('assets/main/css', {sourcemaps: './'}),
+        livereload()
+    ], handleError(done));
+}
+
+function zipper(done) {
+    var targetDir = 'dist/';
+    var themeName = require('./package.json').name;
+    var filename = themeName + '.zip';
+
+    pump([
+        src([
+            '**',
+            '!node_modules', '!node_modules/**',
+            '!dist', '!dist/**'
+        ]),
+        zip(filename),
+        dest(targetDir)
+    ], handleError(done));
+}
+
+const cssWatcher = () => watch('./assets/main/sass/**/**', css);
+const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs', '!node_modules/**/*.hbs'], hbs);
+const watcher = parallel(cssWatcher, hbsWatcher);
+const build = series(css);
+const dev = series(build, serve, watcher);
+
+exports.build = build;
+exports.zip = series(build, zipper);
+exports.default = dev;
