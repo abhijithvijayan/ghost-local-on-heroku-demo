078dba96ffd818cf21df31fc8cc952b94afe03a9
Swastik nath <swastik.nath@live.com>
2019-08-12 22:22:23 +0530

    update server.js for a single dyno support, this does not scale

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 var ghost = require('ghost');
-var express = require('express');
-var urlService = require('./node_modules/ghost/core/server/services/url');
-var parentApp = express();
 
 // Run a single Ghost process
 ghost()
-  .then(function (ghostServer) {
-    ////////////////////////////////////////////////////////////////
-    // this is what you need to get subdirectories working properly!
-    // e.g. https://www.website.com/blog
-    parentApp.use(urlService.utils.getSubdir(), ghostServer.rootApp);
-    ghostServer.start(parentApp);
-    ////////////////////////////////////////////////////////////////
-  })
+  .then( ghostServer => ghostServer.start() )
   .catch( error => {
     console.error(`Ghost server error: ${error.message} ${error.stack}`);
     process.exit(1);
